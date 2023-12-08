160a31c743621ff00f832abfe23aeb27ec58e95f
Kaleb Wondwossen <kalebwondwossent@gmail.com>
2023-12-08 09:46:45 +0000

    new printf

--- _printf.c
+++ _printf.c
@@ -1,66 +1,55 @@
+#include <stdarg.h>
 #include "main.h"
-
-void print_buffer(char buffer[], int *buff_index);
+#include <stddef.h>
+#include <stdio.h>
 
 /**
- * _printf - the function that works as printf update 1
- * @format: the formated text to be printed
+ * _printf - prints formated text.
+ * @format: the text to be printed
  *
- * Return: printed chars
+ * Return: the character printed
  */
-
-
 int _printf(const char *format, ...)
 {
-	int i;
-	int count_char = 0, printed = 0;
-	va_list list;
+	int count = 0, i;
+	va_list data;
 
+	va_start(data, format);
 	if (format == NULL)
 		return (-1);
-
-	va_start(list, format);
-	for (i = 0; format && format[i] != '\0'; i++)
+	for (i = 0; format[i] != '\0'; i++)
 	{
 		if (format[i] != '%')
 		{
-			_putchar(format[i]);
-			count_char++;
+			count += _putchar(format[i]);
 		}
-		else
+		else if (format[i] == '%' && format[i + 1] != ' ')
 		{
-			i++;
-			printed = handle_print(format, &i, list);
-			if (printed == -1)
-				return (-1);
-			count_char += printed;
+			switch (format[i + 1])
+			{
+				case 'c':
+					count += _putchar(va_arg(data, int));
+					break;
+				case 's':
+					count += print_string(va_arg(data, char *));
+					break;
+				case '%':
+					count += _putchar('%');
+					break;
+				case 'i':
+					count += print_decimal(va_arg(data, int));
+					break;
+				case 'd':
+					count += print_decimal(va_arg(data, int));
+					break;
+				default:
+					count += _putchar(format[i]);
+					i--;
+			}
+			i += 1;
 		}
 	}
-
-	va_end(list);
-	return (count_char);
+	va_end(data);
+	return (count);
 }
 
-/**
- * handle_print - prints an argumetn based on it type
- * @fmt: the fomated string which is argumet to printf
- * @list: The list of argument to  be printed
- * @index: the index
- * Return: the prited character count
- */
-int handle_print(const char *fmt, int *index,va_list list)
-{
-	int j, printed = -1;
-
-	fmt_type format_functions[] = {
-		{'c', print_char}, {'s', print_string}, {'%', print_percent},
-		{'d', print_int}, {'i', print_int}, {'b', print_binary},
-		{'u', print_unsigned}, {'o', print_octal}, {'x', print_hexa_lower},
-		{'X', print_hexa_upper}, {'\0', NULL}
-	};
-
-	for (j = 0; format_functions[j].fmt != '\0'; j++)
-		if (fmt[*index] == format_functions[j].fmt)
-			return (format_functions[j].fn(list));
-	return (printed);
-}
